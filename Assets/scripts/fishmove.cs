96ca55787bece641f864046b4439cc1ff28cd039
Luxe Julian <S4165444@student.rmit.edu.au>
2026-04-22 12:20:55 +1000

    fish moving
    the fish moves randomly now but im still working on the speed to get more realistic

--- Assets/scripts/fishmove.cs
+++ Assets/scripts/fishmove.cs
@@ -1,17 +1,27 @@
 using UnityEngine;
+using System;
 
 public class fishmove : MonoBehaviour
 {
-    public Transform destination;
+    Vector3 AimPosition;
+    public GameObject toFollow;
+    public int speed;
 
     void Start()
     {
-        
+        AimPosition = toFollow.transform.position;
     }
 
-    // Update is called once per frame
+    public void SetNewPosition(Vector3 newPostion)
+    {
+        AimPosition = newPostion;
+    }
+
     void Update()
     {
+        AimPosition = toFollow.transform.position;
+  
+        this.transform.position = Vector3.MoveTowards(this.transform.position, AimPosition, Time.deltaTime * speed);
         
     }
 }
